9d5da87e040f375a126c0f6e22be4553351b10e2
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2022-12-03 19:40:27 +0700

    Commit Pertama

---
+++ src/main/java/ws/a/praktikum3/Praktikum3Application.java
@@ -0,0 +1,13 @@
+package ws.a.praktikum3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/a/praktikum3/Praktikum3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.praktikum3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
